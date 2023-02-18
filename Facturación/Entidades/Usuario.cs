b87207cd39d6b7ff2898bf197012f0a0b696a9f8
Aldo-Becker <113803505+Aldo-Becker@users.noreply.github.com>
2023-02-18 16:12:00 -0600

    creación facturación

---
+++ Facturación/Entidades/Usuario.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Usuario
+    {
+        public string CodigoUsuario { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Contraseña { get; set; }
+
+        public string Rol { get; set; }
+
+        public string Correo { get; set; }
+
+        public DateTime FechaCreacion { get; set; }
+
+        public bool EstaActivo { get; set; }
+
+        public Usuario()
+        {
+
+        }
+        public Usuario(string codigoUsuario, string nombre, string contraseña, string rol, string correo, DateTime fechaCreacion, bool estaActivo)
+        {
+            CodigoUsuario = codigoUsuario;
+            Nombre = nombre;
+            Contraseña = contraseña;
+            Rol = rol;
+            Correo = correo;
+            FechaCreacion = fechaCreacion;
+            EstaActivo = estaActivo;
+        }
+    }
+}
---
+++ Facturación/Vista/Login.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vista
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void CancelarButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void AceptarButton_Click(object sender, EventArgs e)
+        {
+            if (UsuarioTextBox.Text == String.Empty)
+            {
+                errorProvider1.SetError(UsuarioTextBox, "Ingrese un Usuario");
+                return;
+            }
+            errorProvider1.Clear();
+            if (string.IsNullOrEmpty(ContraseñaTextBox.Text))
+            {
+                errorProvider1.SetError(ContraseñaTextBox, "Ingrese una Contraseña");
+                ContraseñaTextBox.Focus();
+                return;
+            }
+            errorProvider1.Clear();
+            
+        }
+    }
+}
